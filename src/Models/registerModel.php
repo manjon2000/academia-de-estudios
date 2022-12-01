77916741e0466416d90088eba717df171d8b39c1
manjon2000m <albertmanjon123@gmail.com>
2022-12-01 20:55:33 +0100

    Add Monolog in file registerModel.php

--- src/Models/registerModel.php
+++ src/Models/registerModel.php
@@ -1,17 +1,30 @@
 <?php
 
+use Monolog\Handler\StreamHandler;
+use Monolog\Level;
+use Monolog\Logger;
+
 require_once __DIR__ . '/../Utils/DB.php';
 
 class registerModel {
 
     public static function setUser($args) {
+        /**
+         * Logger
+         */
+        $loggerLoginInfo = new Logger('login_info');
+        $loggerLoginError = new Logger('login_error');
+        $loggerLoginInfo->pushHandler(new StreamHandler(__DIR__ . '/../../logs/register/log.log', Level::Info));
+        $loggerLoginError->pushHandler(new StreamHandler(__DIR__ . '/../../logs/register/log.log', Level::Error));
 
         try {
             $conexion = new DB();
             $querySend = $conexion->connect()->query("INSERT INTO users (`name`, `last_name`, `email`, `password`) VALUES('" . $args['name'] . "', '" . $args['lastName'] . "', '" . $args['email'] . "', '" . $args['password'] . "')");
+            $loggerLoginInfo->info('BBDD: El usuario '.$args['email'].' se ha registrado');
             return true;
         } catch (Exception $e) {
             echo 'Excepción capturada: ', $e->getMessage(), "\n";
+            $loggerLoginError->error('BBDD: Error databases el email '.$args['email'].'ha tenido un error al registrarse'.$e->getMessage());
         }
 
         return null;
